d8b9b4a37d1fc419372b5f108a4cbc74156b878e
msalvadores <msalvadores@gmail.com>
2014-06-25 17:00:10 -0700

    ipython sparql console backend ...

---
+++ dots/ipython/profile_sparql/ipython_config.py
@@ -0,0 +1,11 @@
+c = get_config()
+c.InteractiveShellApp.exec_lines = [
+'from http_sparql_client import *',
+'import os',
+'local = SPARQL("http://localhost:9000/saprql/")',
+'prod_CARE = SPARQL(os.environ[\'SPARQL_BP_PRD\'])',
+'stage = SPARQL(os.environ[\'SPARQL_BP_STG\'])',
+'print "prod_CARE.epr=%s"%prod_CARE.epr',
+'print "stage.epr=%s"%stage.epr',
+'print "local.epr=%s"%local.epr'
+]
