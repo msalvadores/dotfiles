a42083786a07edcbb20732da3686ab22a13722ec
msalvadores <msalvadores@gmail.com>
2014-07-22 09:34:08 -0700

    correction to sparql endpoint path

--- dots/ipython/profile_sparql/ipython_config.py
+++ dots/ipython/profile_sparql/ipython_config.py
@@ -2,7 +2,7 @@ c = get_config()
 c.InteractiveShellApp.exec_lines = [
 'from http_sparql_client import *',
 'import os',
-'local = SPARQL("http://localhost:9000/saprql/")',
+'local = SPARQL("http://localhost:9000/sparql/")',
 'prod_CARE = SPARQL(os.environ[\'SPARQL_BP_PRD\'])',
 'stage = SPARQL(os.environ[\'SPARQL_BP_STG\'])',
 'print "prod_CARE.epr=%s"%prod_CARE.epr',
